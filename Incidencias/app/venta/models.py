c2f8e75382a066248520d743e8d1486a2fe3dd62
ATC <soporte@atc.cl>
2026-05-06 17:47:53 -0400

    Auto update

--- Incidencias/app/venta/models.py
+++ Incidencias/app/venta/models.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from sqlalchemy import DateTime, Index, String, func
 from sqlalchemy.orm import Mapped, mapped_column
 
-from app.core.db import Base
+from app.database import Base
 
 
 class VentaCliente(Base):
